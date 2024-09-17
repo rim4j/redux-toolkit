f80c960de44c1cef044fb81a58dcf8de1c5d5ca0
rim4j <amirhosseinjalali818@gmail.com>
2024-09-17 10:39:11 +0330

    config daisyUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggle = () => {
+    return theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div>
+    <div data-theme={theme}>
       <p className='text-6xl'>hello world</p>
+      <button onClick={toggle} className='btn '>
+        Button
+      </button>
+      <article className='prose'>
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+      <article className='prose'>
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+        fuga! Nam odio recusandae in, incidunt impedit ipsam consequatur quae.
+        Ea, praesentium! Sequi, inventore magni ullam eum libero ex cupiditate?
+        Mollitia!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+        fuga! Nam odio recusandae in, incidunt impedit ipsam consequatur quae.
+        Ea, praesentium! Sequi, inventore magni ullam eum libero ex cupiditate?
+        Mollitia!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+        fuga! Nam odio recusandae in, incidunt impedit ipsam consequatur quae.
+        Ea, praesentium! Sequi, inventore magni ullam eum libero ex cupiditate?
+        Mollitia!
+      </p>
     </div>
   );
 }
