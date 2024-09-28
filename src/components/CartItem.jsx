4578ac4ca92fe4aa75bd0ee0916a27091bd1b1b7
rim4j <amirhosseinjalali818@gmail.com>
2024-09-28 09:08:26 +0330

    increase and decrease items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,11 @@
 import { FaArrowUp } from "react-icons/fa";
 import { FaArrowDown } from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import { removeItem } from "../feature/cart/cartSlice";
+import { removeItem, decrease, increase } from "../feature/cart/cartSlice";
 
 const CartItem = ({ item }) => {
   const dispatch = useDispatch();
+  const id = item.id;
   return (
     <div className='card   lg:card-side bg-base-100 shadow-xl m-8  '>
       <figure className='w-32'>
@@ -25,11 +26,17 @@ const CartItem = ({ item }) => {
       </div>
 
       <div className='flex-col text-center justify-center align-middle p-6'>
-        <button className='btn block'>
+        <button
+          onClick={() => dispatch(increase({ id }))}
+          className='btn block'
+        >
           <FaArrowUp />
         </button>
         <p className='m-0  py-3 font-bold'>{item.amount}</p>
-        <button className='btn block'>
+        <button
+          onClick={() => dispatch(decrease({ id }))}
+          className='btn block'
+        >
           <FaArrowDown />
         </button>
       </div>
